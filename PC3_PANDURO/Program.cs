53e12fe2169431409bf4b7bdc8daf8051f6e75c3
MarceloPA <genialixzz@gmail.com>
2025-05-24 01:29:45 -0500

    Vista titulos

--- PC3_PANDURO/Program.cs
+++ PC3_PANDURO/Program.cs
@@ -1,7 +1,10 @@
+using PC3_PANDURO.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient<JsonPlaceholderService>();
 
 var app = builder.Build();
 
